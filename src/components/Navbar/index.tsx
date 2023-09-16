c2abd0559199a2ac6f709a97fab3f952fde8cf6d
metablog <khu10735@naver.com>
2023-09-16 15:08:54 +0900

    feat: UI navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,36 @@
+import type { FC } from "react";
+import {
+  BellOutlined,
+  LoginOutlined,
+  SearchOutlined,
+  ShoppingCartOutlined,
+} from "@ant-design/icons";
+const iconStyle: string = "cursor-pointer text-[20px]";
+
+const Navbar: FC = () => {
+  return (
+    <div className="w-[80%] m-auto flex justify-between h-[90px] items-center border-b border-[#46A358]">
+      <div>
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/aema-image-upload.appspot.com/o/greenshop%2Ficons%2Flogo.svg?alt=media&token=fc9659d6-f435-43b9-a624-8b0d3a574baa"
+          alt="logo"
+        />
+      </div>
+      <div className="flex gap-4">
+        <h3 className="cursor-pointer">HOME</h3>
+        <h3 className="cursor-pointer">Blog</h3>
+      </div>
+      <div className="flex gap-[30px] items-center">
+        <SearchOutlined className={`${iconStyle}`} />
+        <BellOutlined className={`${iconStyle}`} />
+        <ShoppingCartOutlined className={`${iconStyle}`} />
+        <button className="text-white w-[100px] h-[35px] bg-[#46A358] flex justify-center items-center rounded-md gap-2">
+          <LoginOutlined />
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import Navbar from "./components/Navbar";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
-root.render(<App />);
+root.render(<Navbar />);
